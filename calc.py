c6e9f2ceca8c0435081de5d2397450dab6815ed5
chpdrx <thewinniex@gmail.com>
2021-07-14 20:37:55 +0300

    calculator

--- calc.py
+++
@@ -1,4 +0,0 @@
-def calculator(x,y,z):
-    return eval(x+z+y)
-
-calculator('2','2','+')
--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 from kivy.app import App
-from kivy.uix.button import Button
 
 class Calculator(App):
-    x = 0
-    def build(self):
-        b1 = Button(text = '1', font_size = 20)
-        b1.background_color = [0,1,0,1]
-        b1.bind(on_press = callback)
-        return b1
-
-    def callback(self):
-        return b1.text
-
-    def calculator(self, callback(), y, z):
-        return eval(x + z + y)
+    pass
 
 if __name__ == '__main__':
     Calculator().run()
