079d38bfbb2d8b3cd38cf615ba16778ddd05e0bd
Osujdenie <osujdenie@yandex.ru>
2021-07-16 20:54:26 +0300

    .

---
+++ random.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+#from kivy.config import Config
+#from kivy.uix.boxlayout import BoxLayout
+#Config.set('graphics', 'resizable', False)
+#Config.set('graphics', 'width', '201')
+#Config.set('graphics', 'height', '50')
+
+#class Layout(BoxLayout):
+#    def press_button_random(self):
+#        self.text_input.text = self.press_button_random()
+
+#    def press_button_clear(self):
+#        self.text_input.text = ''
+
+class RandomNumber(App):
+    pass
+#    def build(self):
+#        return BoxLayout()
+
+if __name__ == '__random__':
+    RandomNumber().run()
