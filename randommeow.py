06caad705e5e2b551d1379181bfe22ea8a800bdb
Osujdenie <osujdenie@yandex.ru>
2021-07-16 22:04:33 +0300

    push

--- randommeow.py
+++ randommeow.py
@@ -1,21 +1,23 @@
 from kivy.app import App
 from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
 import random
+Builder.load_file('Randomkivy.kv')
 Config.set('graphics', 'resizable', False)
-Config.set('graphics', 'width', '201')
-Config.set('graphics', 'height', '50')
+Config.set('graphics', 'width', '251')
+Config.set('graphics', 'height', '100')
 
-class randomkivy(BoxLayout):
+class Randomkivy(BoxLayout):
     def press_button_random(self):
-        self.text_input.text = random.randrange(1, 100)
+        self.text_input.text = str(random.randrange(1, 100))
 
     def press_button_clear(self):
         self.text_input.text = ''
 
 class RandomApp(App):
     def build(self):
-        return randomkivy()
+        return Randomkivy()
 
-if __name__ == '__randommeow__':
+if __name__ == '__main__':
     RandomApp().run()
